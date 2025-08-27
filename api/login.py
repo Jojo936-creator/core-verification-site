531dee3dbab9090321fa0cf23119717ae2eea9b3
Jojo936-creator <jojo530278@gmail.com>
2025-08-27 10:38:48 +0200

    Update login.py

--- api/login.py
+++ api/login.py
@@ -1,19 +1,22 @@
 import os
-from urllib.parse import urlencode
+import requests
 
-def handler(request):
-    base_url = "https://apis.roblox.com/oauth/v1/authorize"
-    params = {
-        "response_type": "code",
+def exchange_code_for_token(code):
+    token_url = "https://apis.roblox.com/oauth/v1/token"
+    data = {
         "client_id": os.getenv("CLIENT_ID"),
-        "redirect_uri": os.getenv("REDIRECT_URI"),
-        "scope": "openid"
+        "client_secret": os.getenv("CLIENT_SECRET"),
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": os.getenv("REDIRECT_URI")
     }
 
-    redirect_url = f"{base_url}?{urlencode(params)}"
-    return {
-        "statusCode": 302,
-        "headers": {
-            "Location": redirect_url
-        }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
     }
+
+    response = requests.post(token_url, data=data, headers=headers)
+    print("Status:", response.status_code)
+    print("Text:", response.text)  
+
+    return response.json()
